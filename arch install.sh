2c85c56f2d2560195a9f593b30e294b02ec02f85
raunakwete43 <104648854+raunakwete43@users.noreply.github.com>
2023-02-18 12:31:22 +0530

    Create arch install.sh

---
+++ arch install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Update the system clock
+timedatectl set-ntp true
+
+# Partition the disk
+# Replace /dev/sda with your disk device
+(
+  echo o; # Create a new empty GPT partition table
+  echo n; # Create a new partition
+  echo ; # Partition number (default is 1)
+  echo ; # First sector (default is 2048)
+  echo +512M; # Last sector for EFI system partition
+  echo y; # Set partition as bootable
+  echo t; # Change partition type
+  echo 1; # EFI System partition
+  echo n; # Create a new partition
+  echo ; # Partition number (default is 2)
+  echo ; # First sector (default is next free sector)
+  echo ; # Last sector (default is entire disk)
+  echo w; # Write changes to disk
+) | fdisk /dev/sda
+
+# Format the partitions
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+
+# Mount the partitions
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+# Install the base system
+pacstrap /mnt base linux linux-firmware
+
+# Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Chroot into the new system
+arch-chroot /mnt /bin/bash <<EOF
+
+# Set the timezone
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+hwclock --systohc
+
+# Set the hostname
+echo "arch" > /etc/hostname
+
+# Generate the locale
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# Set the root password
+echo "root:password" | chpasswd
+
+# Install Xorg and other packages
+pacman -S xorg xorg-xinit --noconfirm
+
+# Install the systemd boot manager
+bootctl install
+
+# Configure the boot manager
+cat <<EOF2 > /boot/loader/loader.conf
+default arch
+timeout 0
+EOF2
+
+cat <<EOF2 > /boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=/dev/sda2 rw
+EOF2
+
+EOF
+
+# Unmount partitions and reboot
+umount -R /mnt
+reboot
